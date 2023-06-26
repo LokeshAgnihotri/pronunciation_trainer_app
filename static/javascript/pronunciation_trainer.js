ffcbb0e1cf2624d62b9720ea86c6ccafbf770cb2
Lokesh <lokesh.agnihotri@outlook.com>
2023-06-26 10:48:52 +0200

    updaetd pronunciation

--- static/javascript/pronunciation_trainer.js
+++ static/javascript/pronunciation_trainer.js
@@ -1,41 +1,36 @@
-
-const _button_pronounce_trainer = document.getElementById("button_pronounce_trainer")
-const _reftext = document.getElementById("reftext")
-const audio = document.getElementById("audio`")
+const _button_pronounce_trainer = document.getElementById("button_pronounce_trainer");
+const _reftext = document.getElementById("reftext");
+const audio = document.getElementById("audio");
 
 _button_pronounce_trainer.onclick = function () {
-    next_button_audio.play()
+    next_button_audio.play();
 
-    fetch("/pronunciation_trainer",
-        {
+    // Check if _reftext value is a phonetics string
+    const phoneticsRegex = /^[A-Za-z\s]+$/;
+    if (!phoneticsRegex.test(_reftext.value)) {
+        // If it is not a phonetics string, proceed with the fetch request
+        fetch("http://127.0.0.1:5000/pronunciation_trainer", {
             method: 'POST',
             headers: {
                 'Content-type': 'application/json',
                 'Accept': 'application/json'
             },
-
             body: JSON.stringify(_reftext.value)
         })
-        .then(response =>
-        {
-        if (response.ok)
-        {
-            return response.json()
-        } else
-        {
-            alert("something is wrong")
-        }
-    }).then(jsonResponse => {
-        _reftext.value = jsonResponse['phenome'];
-
-
-
-        document.getElementById("audioPlayer").src = jsonResponse['sound'];
-        audioPlayer.play();  // Play the audio
-
-
-
-        }
-    ).catch((err) => console.error(err));
-
-}
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("Something went wrong");
+            }
+        })
+        .then(jsonResponse => {
+            _reftext.value = jsonResponse['phenome'];
+            document.getElementById("audioPlayer").src = jsonResponse['sound'];
+            audioPlayer.play(); // Play the audio
+        })
+        .catch(error => {
+            alert(error.message);
+        });
+    }
+};
